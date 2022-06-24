7e296b2f7df40834dc99313e7405974a002f3670
”Vadim <vadimka.gapon@gmail.com>
2022-06-25 01:58:12 +0600

    add docker

--- roulette/roulette_main/apps.py
+++ roulette/roulette_main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RouletteMainConfig(AppConfig):
+class RouletteMainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'roulette_main'
