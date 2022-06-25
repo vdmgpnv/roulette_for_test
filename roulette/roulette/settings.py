e9c7da6cdb30ffa00082344f38c24ed31633f053
”Vadim <vadimka.gapon@gmail.com>
2022-06-25 09:24:44 +0600

    hh

--- roulette/roulette/settings.py
+++ roulette/roulette/settings.py
@@ -29,14 +29,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT'
-]
 
 # Application definition
 
